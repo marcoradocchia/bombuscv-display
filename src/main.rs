4ff01e997696cde82437c2cd065b1a4fa806820f
Marco Radocchia <marco.radocchia@outlook.com>
2022-07-13 18:59:23 +0200

    Implemented I2C display

--- src/main.rs
+++ src/main.rs
@@ -1,60 +1,122 @@
+use clap::Parser;
+use embedded_graphics::{
+    mono_font::{ascii::FONT_6X9, MonoTextStyle},
+    pixelcolor::BinaryColor,
+    prelude::*,
+    text::Text,
+};
 use interfaces::{Interface, Kind};
 use procfs::process::all_processes;
+use rppal::i2c;
+use sh1106::{prelude::*, Builder};
+use signal_hook::{consts::SIGINT, flag::register};
 use std::{
     fmt::{self, Display, Formatter},
     fs::File,
     io::{self, BufRead, Read},
-    net::IpAddr,
+    net::{IpAddr, Ipv4Addr},
+    ops::Index,
     process,
-    sync::mpsc,
+    sync::{
+        atomic::{AtomicBool, Ordering},
+        mpsc, Arc,
+    },
     thread,
+    time::Duration,
 };
 
-// const INTERFACE_NAME: &str = "wlan0";
-// const THERMAL_ZONE: &str = "/sys/class/hwmon/hwmon3/temp1_input";
-const INTERFACE_NAME: &str = "wlan0";
-const THERMAL_ZONE: &str = "/sys/class/thermal/thermal_zone0/temp";
+/// CLI tool to print bombuscv-rs information to I2C display.
+#[derive(Parser, Debug)]
+#[clap(
+    author = "Marco Radocchia <marco.radocchia@outlook.com>",
+    version,
+    about,
+    long_about = None
+)]
+struct Args {
+    /// CPU `temp` file path.
+    #[clap(
+        short,
+        long,
+        value_parser,
+        default_value = "/sys/class/thermal/thermal_zone0/temp"
+    )]
+    thermal: String,
+
+    /// Network interface name for local IPv4 stamp.
+    #[clap(short, long, value_parser, default_value = "wlan0")]
+    interface: String,
+
+    /// GPIO pin for SCL (I2C) connection.
+    #[clap(long, value_parser)]
+    scl: u8,
+
+    /// GPIO pin for SDA (I2C) connection.
+    #[clap(long, value_parser)]
+    sda: u8,
+}
 
 /// Enum representing handled runtime errors.
-enum ErrorKind {
+enum ErrorKind<'a> {
     /// Occurs when network interface is not found.
-    InterfaceNotFound,
+    InterfaceNotFound(&'a str),
+
     /// Occurs when network interface was not assigned an IPv4.
-    IPv4NotFound,
+    IPv4NotFound(&'a str),
+
     /// Occurs when file could not be read.
-    InaccessibleFile(&'static str),
+    InaccessibleFile(&'a str),
+
     /// Occurs when list of system processes could not be retrieved.
     ProcListErr,
+
     /// Occurs when fed invalid humidity & temperature data.
     InvalidHumTemp,
+
     /// Occurs when invalid input was piped to the program.
     InvalidInput,
+
+    /// Occurs when unable to register SIGINT event handler.
+    SigIntHandlerErr,
+
+    /// Occurs when unable to setup I2C bus.
+    I2cSetupErr,
+
+    /// Occurs when unable to write to I2C.
+    I2cWriteErr,
 }
 
 /// Implementing Display trait for ErrorKind enum.
-impl Display for ErrorKind {
+impl Display for ErrorKind<'_> {
     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
         match self {
-            Self::IPv4NotFound => format!("no IPv4 found for `{INTERFACE_NAME}` network interface"),
-            Self::InterfaceNotFound => format!("`{INTERFACE_NAME}` network interface not found"),
-            Self::InaccessibleFile(filename) => format!("impossible to access `{filename}` file"),
-            Self::ProcListErr => "unable to retrieve process list".to_string(),
-            Self::InvalidHumTemp => {
-                "invalid input data format; please use `<hum>,<temp>` instead".to_string()
+            Self::IPv4NotFound(interface) => {
+                write!(f, "no IPv4 found for `{interface}` network interface")
             }
-            Self::InvalidInput => "invalid input piped to the program".to_string(),
+            Self::InterfaceNotFound(interface) => {
+                write!(f, "`{interface}` network interface not found")
+            }
+            Self::InaccessibleFile(filename) => write!(f, "impossible to access `{filename}` file"),
+            Self::ProcListErr => write!(f, "unable to retrieve process list"),
+            Self::InvalidHumTemp => {
+                write!(f, "invalid input format; please use `<hum>,<temp>` instead")
+            }
+            Self::InvalidInput => write!(f, "invalid input piped to the program"),
+            Self::SigIntHandlerErr => write!(f, "unable to register SIGINT event handler"),
+            Self::I2cSetupErr => write!(f, "unable to setup I2C bus"),
+            Self::I2cWriteErr => write!(f, "unable to write to I2C display"),
         }
-        .fmt(f)
     }
 }
 
 /// Humidity and Temperature measure.
+#[derive(Debug)]
 struct Measure {
     humidity: f32,
     temperature: f32,
 }
 
-impl Measure {
+impl<'a> Measure {
     // Construct `Measure`.
     fn new(humidity: f32, temperature: f32) -> Self {
         Self {
@@ -63,13 +125,12 @@ impl Measure {
         }
     }
 
-    /// Construct `Measure` from stdin data.
-    fn from_str(data: &str) -> Result<Self, ErrorKind> {
-        let splits = data.split(',').collect::<Vec<&str>>();
+    /// Construct `Measure` from string <hum,temp>.
+    fn from_str(data: &str) -> Result<Self, ErrorKind<'a>> {
+        let splits: Vec<f32> = data.split(',').map(|val| val.parse().unwrap()).collect();
 
         if splits.len() == 2 {
-            let mut splits = splits.iter().map(|val| val.parse::<f32>().unwrap());
-            Ok(Measure::new(splits.next().unwrap(), splits.next().unwrap()))
+            Ok(Measure::new(*splits.index(0), *splits.index(1)))
         } else {
             Err(ErrorKind::InvalidHumTemp)
         }
@@ -83,94 +144,165 @@ impl Display for Measure {
 }
 
 /// Retrieve local IPv4 of network interface.
-fn local_ipv4() -> Result<IpAddr, ErrorKind> {
-    let interface = match Interface::get_by_name(INTERFACE_NAME)
-        .expect("failed to get {INTERFACE_NAME} info")
+fn local_ipv4(interface: &str) -> Result<IpAddr, ErrorKind> {
+    if let Some(interface) =
+        Interface::get_by_name(interface).expect("failed to get {interface} info")
     {
-        Some(interface) => interface,
-        None => return Err(ErrorKind::InterfaceNotFound),
-    };
-
-    for addr in &interface.addresses {
-        if addr.kind == Kind::Ipv4 {
-            return Ok(addr.addr.unwrap().ip());
+        for addr in &interface.addresses {
+            if addr.kind == Kind::Ipv4 {
+                return Ok(addr.addr.unwrap().ip());
+            }
         }
+    } else {
+        return Err(ErrorKind::InterfaceNotFound(interface));
     }
 
-    Err(ErrorKind::IPv4NotFound)
+    Err(ErrorKind::IPv4NotFound(interface))
 }
 
 /// Retrieve CPU package temperature.
-fn cpu_temp() -> Result<f32, ErrorKind> {
+fn cpu_temp(thermal_zone: &str) -> Result<f32, ErrorKind> {
     let mut temp = String::new();
 
-    match File::open(THERMAL_ZONE) {
-        Ok(mut file) => file
-            .read_to_string(&mut temp)
-            .expect("unable to read `{THERMAL_ZONE}` file"),
-        Err(_) => return Err(ErrorKind::InaccessibleFile(THERMAL_ZONE)),
-    };
+    if let Ok(mut file) = File::open(thermal_zone) {
+        file.read_to_string(&mut temp)
+            .expect("unable to read `{thermal_zone}` file");
+        return Ok(temp
+            .trim()
+            .parse::<f32>()
+            .expect("unable to parse `{thermal_zone}` content to f32")
+            / 1000.0);
+    }
 
-    Ok(temp
-        .trim()
-        .parse::<f32>()
-        .expect("unable to parse `{THERMAL_ZONE}` content to f32")
-        / 1000.0)
+    return Err(ErrorKind::InaccessibleFile(thermal_zone));
 }
 
 /// Check for running process returnin bool whether the process is running or not.
 fn pgrep(name: &str) -> Result<bool, ErrorKind> {
-    let proc_list = match all_processes() {
-        Ok(proc_list) => proc_list,
-        Err(_) => return Err(ErrorKind::ProcListErr),
-    };
-
-    for proc in proc_list {
-        if let Ok(exe_path) = proc.unwrap().exe() {
-            if exe_path.file_stem().unwrap() == name {
-                return Ok(true);
+    if let Ok(proc_list) = all_processes() {
+        for proc in proc_list {
+            if let Ok(exe_path) = proc.unwrap().exe() {
+                if exe_path.file_stem().unwrap() == name {
+                    return Ok(true);
+                }
             }
         }
+    } else {
+        return Err(ErrorKind::ProcListErr);
     }
 
     Ok(false)
 }
 
+struct I2cDisplay {
+    disp: GraphicsMode<I2cInterface<i2c::I2c>>,
+}
+
+impl I2cDisplay {
+    /// Initialize & setup SH1106 I2C display.
+    fn new<'a>() -> Result<Self, ErrorKind<'a>> {
+        // TODO: change here to let the user specify custom pins
+        if let Ok(i2c) = i2c::I2c::new() {
+            Ok(Self {
+                disp: Builder::new().connect_i2c(i2c).into(),
+            })
+        } else {
+            Err(ErrorKind::I2cSetupErr)
+        }
+    }
+
+    /// Refresh display screen.
+    fn refresh_display<'a>(&mut self, lines: &str) -> Result<(), ErrorKind<'a>> {
+        // WARNING: it would be a better practice to implement From trait for ErrorKind in order to
+        // convert DI::Error to ErrorKind variant and use ? operator to return error.
+        if self.disp.init().is_err() {
+            return Err(ErrorKind::I2cWriteErr);
+        }
+        if self.disp.flush().is_err() {
+            return Err(ErrorKind::I2cWriteErr);
+        }
+
+        // Draw text to display.
+        if Text::new(
+            lines,
+            Point::zero(),
+            MonoTextStyle::new(&FONT_6X9, BinaryColor::On),
+        )
+        .draw(&mut self.disp)
+        .is_err()
+        {
+            return Err(ErrorKind::I2cWriteErr);
+        }
+
+        if self.disp.flush().is_err() {
+            return Err(ErrorKind::I2cWriteErr);
+        }
+        Ok(())
+    }
+}
+
 /// Run application and catch errors.
-fn run() -> Result<(), ErrorKind> {
-    let (tx, rx) = mpsc::channel();
+fn run(args: &Args) -> Result<(), ErrorKind> {
+    // Register signal-hook for SIGINT (Ctrl-C) events: in this case error is unrecoverable.
+    let term = Arc::new(AtomicBool::new(false));
+    if register(SIGINT, Arc::clone(&term)).is_err() {
+        return Err(ErrorKind::SigIntHandlerErr);
+    };
+
+    // Sender/Receiver for measure values.
+    let (tx_measure, rx_measure) = mpsc::channel();
 
     let measure_handle = thread::spawn(move || -> Result<(), ErrorKind> {
         // Read data from stdin (used in this case to pipe from datalogger, program).
         // https://github.com/marcoradocchia/datalogger
         for line in io::stdin().lock().lines() {
-            match line {
-                Ok(line) => {
-                    tx.send(Measure::from_str(&line)?)
-                        .expect("unable to send hum_temp data between threads");
-                }
-                Err(_) => return Err(ErrorKind::InvalidInput),
-            };
+            if let Ok(line) = line {
+                tx_measure
+                    .send(Measure::from_str(&line))
+                    .expect("unable to send hum_temp data between threads");
+            } else {
+                return Err(ErrorKind::InvalidInput);
+            }
         }
 
         Ok(())
     });
 
-    for msg in rx {
-        println!(
+    // Initialize I2C  display.
+    let mut i2c_display = I2cDisplay::new()?;
+
+    // Grab the first measure.
+    let mut measure: Measure = rx_measure
+        .recv()
+        .expect("unable to receive measure from measure thread")?;
+
+    // Start grabber loop: loop guard is `received SIGINT`.
+    while !term.load(Ordering::Relaxed) {
+        // This sets approx display refresh rate.
+        if let Ok(new_measure) = rx_measure.recv_timeout(Duration::from_millis(2000)) {
+            measure = new_measure?
+        }
+
+        // Refresh I2C display.
+        i2c_display.refresh_display(&format!(
             "{}\nIP: {}\nCPU: {}°C\nBOMBUSCV: {}",
-            msg,
-            local_ipv4()?,
-            cpu_temp()?,
-            if pgrep("bombuscv")? { "running" } else { "--" },
-        );
+            measure,
+            local_ipv4(&args.interface).unwrap_or(IpAddr::V4(Ipv4Addr::UNSPECIFIED)),
+            cpu_temp(&args.thermal)?,
+            if pgrep("bombuscv")? { "running" } else { "--" }
+        ))?;
     }
 
-    measure_handle.join().unwrap()
+    measure_handle
+        .join()
+        .expect("unable to join measure_handle thread")?;
+    Ok(())
 }
 
 fn main() {
-    if let Err(e) = run() {
+    let args = Args::parse();
+
+    if let Err(e) = run(&args) {
         eprintln!("error: {e}");
         process::exit(1);
     }
