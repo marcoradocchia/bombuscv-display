c55e215b4d16d83265cc70f4520b060257db3432
Marco Radocchia <marco.radocchia@outlook.com>
2022-07-22 16:12:44 +0200

    Temporary removed scl/sda options

--- src/args.rs
+++ src/args.rs
@@ -22,11 +22,14 @@ pub struct Args {
     #[clap(short, long, value_parser, default_value = "wlan0")]
     pub interface: String,
 
-    /// GPIO pin for SCL (I2C) connection.
-    #[clap(long, value_parser)]
-    pub scl: u8,
-
-    /// GPIO pin for SDA (I2C) connection.
-    #[clap(long, value_parser)]
-    pub sda: u8,
+    // TODO: add options to let the user choose I2C pins on RaspberryPi 4 (older RaspberryPis don't
+    // support it).
+    //
+    // /// GPIO pin for SCL (I2C) connection.
+    // #[clap(long, value_parser)]
+    // pub scl: u8,
+    //
+    // /// GPIO pin for SDA (I2C) connection.
+    // #[clap(long, value_parser)]
+    // pub sda: u8,
 }
